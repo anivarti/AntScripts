8d82580aa31db2d94abe2d177c300c355bb32e74
anivarti <ananth.nivarti@gmail.com>
2020-09-19 21:16:07 -0700

    updated the intendation

--- python/mylineplot.py
+++ python/mylineplot.py
@@ -1,39 +1,21 @@
 #!/user/bin/env python3 -W ignore
-
 from pandas import DataFrame, read_csv
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
 import sys
- 
 ## Define the default values
 parser = argparse.ArgumentParser()
-parser.add_argument('-csv', action='store',
-                    dest='csvfile',
-                    help='path to csvfile first column x axis',
-                    type=str)
-parser.add_argument('-xlabel', action='store',
-                    dest='xlabel',
-                    help='label for the x axis',
-                    type=str)
-parser.add_argument('-ylabel', action='store',
-                    dest='ylabel',
-                    help='label for the y axis',
-                    type=str)
-parser.add_argument('-output', action='store',
-                    dest='output',
-                    help='name of the output file',
-                    type=str)
-parser.add_argument('-title', action='store',
-                    dest='title',
-                    help='title of the plot',
-                    type=str)
+parser.add_argument('-csv'   , action='store',dest='csvfile',help='path to csvfile first column x axis',type=str)
+parser.add_argument('-xlabel', action='store',dest='xlabel' ,help='label for the x axis',type=str)
+parser.add_argument('-ylabel', action='store',dest='ylabel' ,help='label for the y axis',type=str)
+parser.add_argument('-output', action='store',dest='output' ,help='name of the output file',type=str)
+parser.add_argument('-title', action='store' ,dest='title'  ,help='title of the plot',type=str)
 results = parser.parse_args()
 
 if not (results.csvfile): sys.exit("Error:csv file not defined")
 if not (results.output): results.output = "mylineplot"
 if not (results.title): results.title = "mytitle"
- 
 file = open(results.csvfile,'r')
 df = pd.read_csv(file)
 
